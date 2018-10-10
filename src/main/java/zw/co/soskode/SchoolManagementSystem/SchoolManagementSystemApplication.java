70352ee993aeff81f7641185c82e6a76a1fa8d5c
Edmond T Zinzombe <ezinzombe@hitrac.co.zw>
2018-10-10 09:08:51 +0200

    First commit

---
+++ src/main/java/zw/co/soskode/SchoolManagementSystem/SchoolManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package zw.co.soskode.SchoolManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementSystemApplication.class, args);
+    }
+}
